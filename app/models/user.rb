9e6399aa2ad11f2bf7ebabc30580e7d57bbbce6c
Mark McEahern <mark@mceahern.com>
2014-03-10 18:51:25 -0500

    User#email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   validates :name,
     presence: true,
     length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
-    presence: true
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX }
 end
