0a562dee094da3dbade0f939be1b19e03d438b3b
Mark McEahern <mark@mceahern.com>
2014-03-24 16:40:44 -0500

    More sophisticated regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
     presence: true,
     length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :email,
     presence: true,
     format: { with: VALID_EMAIL_REGEX }
