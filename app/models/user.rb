f8ae742494affb8988378ece928fcf3403231253
Mark McEahern <mark@mceahern.com>
2014-04-07 20:00:31 -0500

    Sign out and other stuff is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,18 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   before_save { email.downcase! }
+  before_create :create_remember_token
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
