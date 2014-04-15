525f0337929f2247b910e47f5d263e9eda870604
Mark McEahern <mark@mceahern.com>
2014-04-14 20:24:19 -0500

    pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    {"user#{SecureRandom.hex(5)}@example.com"}
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{SecureRandom.hex(8)}@example.com" }
     password "foobar"
     password_confirmation "foobar"
   end
